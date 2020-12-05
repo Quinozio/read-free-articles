7213dbb875e0e86ce6db46933af70fe753ae4749
DELL-NICHOLAS\Nicholas <nicholas@mediacy.it>
2020-12-05 19:00:15 +0100

    repubblica standart site

--- repubblica.js
+++ repubblica.js
@@ -19,8 +19,36 @@ function getRepubblicaArticle() {
   return { ampElement: null, paywall: null };
 }
 
-const isRepubblica = window.location.href.indexOf("repubblica") > -1;
-if (isRepubblica) {
+const isRepubblicaStandard = window.location.href.indexOf("repubblica") > -1;
+const isRepubblicaWebapp = window.location.href.indexOf("rep.repubblica") > -1;
+
+if (isRepubblicaStandard && !isRepubblicaWebapp) {
+  const isArticle = document.getElementById("article-body");
+  if (isArticle) {
+    const articleUrl = `${window.location.href}`;
+    fetch(articleUrl).then((response) =>
+      response.text().then((data) => {
+        setTimeout(() => {
+          document.getElementsByTagName("html")[0].innerHTML = data;
+        }, 5000);
+      })
+    );
+  }
+  function getArticleFullText(data) {
+    const initialString = `<div class="story__content" id="article-body">`;
+    const finalString = `<div class="paywall-adagio"`;
+    let article = data.substring(
+      data.indexOf(initialString),
+      data.indexOf(finalString)
+    );
+    article = article.substring(
+      initialString.length,
+      article.length - "</div>".length
+    );
+    return article;
+  }
+}
+if (isRepubblicaWebapp) {
   document.addEventListener("DOMNodeInserted", () => {
     const { ampElement, paywall } = getRepubblicaArticle();
     if (paywall) {
